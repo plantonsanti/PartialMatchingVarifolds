a99b9301ab02079104581e6bf94131808a0e7302
Pierre-Louis Antonsanti <pierre-louis.antonsanti@u-paris.fr>
2021-07-27 13:31:08 +0000

    Updating the experiences

--- example_vascular_trees.py
+++ example_vascular_trees.py
@@ -7,12 +7,11 @@ from IO                           import *
 from registration_wrapper         import register_structure
 
 
-common = './data/surfaces/'
+common = './data/trees/'
 
 """
 The paramters of the registration :
 gamma          -> controls the regularity of the deformations
-factor         -> if one changes the unit of the axes
 sigmaV         -> the largest scale of the deformations 
 sigmaW         -> the scales in the data attachment terms
 max_iter_steps -> number of iteration per scale
@@ -24,35 +23,40 @@ method         -> the data attachment method, possible keywords:
 """
 
 parameters = {
-    "gamma": 10000,
+    "Default": 0,
+    "gamma": 10,
+    "factor": 1,
     "sigmaV": 100,
     "sigmaW": [
+        20,
         10,
         5
     ],
     "max_iter_steps": [
-        100,
-        500
+        1000,
+        1000,
+        1000
     ],
-    "method": "PartialVarifoldLocalNormalized" # "Varifold" # 
+    "method": "Varifold" # "PartialVarifoldLocalNormalized" # 
 }
 
 
-folder2save = './results/surfaces/'
+folder2save = './results/vascular_trees/'
 try_mkdir(folder2save)  
 
-path_CBCT  = common+'/CBCT.npy'
-path_CT    = common+'/CT.npy'
+path_template  = common+'/template.npz'
+path_target    = common+'/target.npz'
 
-V_CBCT_np = np.load(path_CBCT, allow_pickle=True)[0]['points']
-F_CBCT_np = np.load(path_CBCT, allow_pickle=True)[0]['cells'].astype(dtype = 'int32')             
-#V_CBCT_np, F_CBCT_np =  RemoveDuplicates(V_CBCT_np,F_CBCT_np)
 
-V_CT_np = np.load(path_CT, allow_pickle=True)[0]['points']
-F_CT_np = np.load(path_CT, allow_pickle=True)[0]['cells'].astype(dtype = 'int32')
-#V_CT_np, F_CT_np = RemoveDuplicates(V_CT_np,F_CT_np)
+V_template_np = np.load(path_template, allow_pickle=True)['points']
+F_template_np = np.load(path_template, allow_pickle=True)['connections'].astype(dtype = 'int32')             
 
-register_structure(V_CBCT_np, F_CBCT_np, V_CT_np, F_CT_np, folder2save, parameters=parameters)
+V_target_np = np.load(path_target, allow_pickle=True)['points']
+F_target_np = np.load(path_target, allow_pickle=True)['connections'].astype(dtype = 'int32')
 
+
+register_structure(V_template_np, F_template_np, 
+                   V_target_np, F_target_np, 
+                   folder2save, parameters=parameters, structure='Curves')
 
 
