6e8f60e229d52ce0f877be44630478a752762513
plantonsanti <pierre-louis.antonsanti@orange.fr>
2022-08-01 17:41:15 +0000

    Working environment for installation

--- registration/optimization.py
+++ registration/optimization.py
@@ -6,11 +6,8 @@ import torch
 import time
 import pickle
 import os
-import numpy as np
-from scipy.optimize import minimize
 
 params_opt=dict({"lr" : 1,"maxcor" : 10, "gtol" : 1e-6, "tol" : 1e-6, "use_scipy" : True, "method" : 'L-BFGS-B'})
-
 
 def opt(loss,p0,q0, maxiter = 100, folder2save = '',savename = ''):
     """
@@ -21,20 +18,16 @@ def opt(loss,p0,q0, maxiter = 100, folder2save = '',savename = ''):
     maxcor    = params_opt["maxcor"]
     gtol      = params_opt["gtol"]
     tol       = params_opt["tol"]
-    use_scipy = params_opt["use_scipy"] #If use_scipy : perform otpimization with LBFGS on scipy. 
-    method    = params_opt["method"]
-    options   = dict( maxiter = maxiter,
-                      ftol    = tol,
-                      gtol    = gtol,
-                      maxcor  = maxcor        # Number of previous gradients used to approximate the Hessian
-                )
+    max_eval  = 10
 
     loss_dict = {}
 
     loss_dict['A'] = [0]
     loss_dict['E'] = [0]
 
-    optimizer = torch.optim.LBFGS([p0])
+    optimizer = torch.optim.LBFGS([p0], line_search_fn='strong_wolfe', lr = lr, 
+                                  tolerance_grad = gtol, tolerance_change = tol,
+                                  max_eval=max_eval)
     start = time.time()
     print('performing optimization...')
     opt.nit = -1
@@ -57,35 +50,8 @@ def opt(loss,p0,q0, maxiter = 100, folder2save = '',savename = ''):
 
         return L
 
-    # Optimisation using scipy : we need to transfer the data from variable to float64
-    def numpy_closure(vec):
-        vec = lr*vec.astype('float64')
-        numpy_to_model(p0,vec)
-        c =  closure().data.view(-1).cpu().numpy()[0]
-        dvec = model_to_numpy(p0,grad = True)
-        return (c,dvec)
-    
-    def model_to_numpy(p, grad=False) :
-        if grad :
-            tensors = p.grad.data.view(-1).cpu().numpy()
-        else :
-            tensors = p.data.view(-1).cpu().numpy()     
-        return np.ascontiguousarray( np.hstack(tensors) , dtype='float64' )
-    
-    def numpy_to_model(p, vec) :
-        p.data = torch.from_numpy(vec).view(p.data.size()).type(p.data.type())
-
-    if use_scipy :
-        res = minimize( numpy_closure,      # function to minimize
-                model_to_numpy(p0), # starting estimate
-                method  = method,
-                jac     = True,             # matching_problems also returns the gradient
-                options = options)
-        print(res.message)
-        
-    else :
-        for i in range(int(maxiter/20)+1):            # Fixed number of iterations
-            optimizer.step(closure)         # "Gradient descent" step.
+    for i in range(int(maxiter/20)+1):            # Fixed number of iterations
+        optimizer.step(closure)         # "Gradient descent" step.
     
     total_time = round(time.time()-start,2)
     print('Optimization time : ',total_time,' seconds')
